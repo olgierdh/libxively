785ada3a529044598fe0da1cbddf5eeba4fda02b
Ilya Dmitrichenko <ilya@xively.com>
2013-09-03 16:19:31 +0100

    [feature][build] Alternate allocator functions (close #34)

--- src/libxively/xi_allocator_stdlib.h
+++ src/libxively/xi_allocator_stdlib.h
@@ -2,19 +2,32 @@
 // This is part of Xively C library, it is under the BSD 3-Clause license.
 
 /**
- * \file	xi_allocator.c
+ * \file	xi_allocator_stdlib.h
  * \author  Olgierd Humenczuk
  * \brief   Our custom `alloc()` and `free()` [see xi_allocator.h]
  */
 
-#include "xi_allocator.h"
+#ifndef __XI_ALLOCATOR_STDLIB_H__
+#define __XI_ALLOCATOR_STDLIB_H__
 
-void* xi_alloc( size_t b )
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static inline void* xi_alloc( size_t b )
 {
     return ( void* ) malloc( b );
 }
 
-void xi_free( void* p )
+static inline void xi_free( void* p )
 {
     free( p );
 }
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
