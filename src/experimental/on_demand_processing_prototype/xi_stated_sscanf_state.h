1fd263ca0a7f5eb335b2c542c4a044f578d1cb96
Olgierd Humenczuk <olgierdh@gmail.com>
2013-10-09 14:29:32 +0200

    [fix] missed new filed

---
+++ src/experimental/on_demand_processing_prototype/xi_stated_sscanf_state.h
@@ -0,0 +1,10 @@
+#ifndef __XI_STATED_SSCANF_STATE_H__
+#define __XI_STATED_SSCANF_STATE_H__
+
+typedef struct
+{
+    short state; char buffer[ 8 ];unsigned char p; unsigned char i; unsigned char buff_len; unsigned char tmp_i; unsigned char vi;
+} xi_stated_sscanf_state_t;
+
+
+#endif // __XI_STATED_SSCANF_STATE_H__
