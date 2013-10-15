45f629a8f08755ab8d1e23875669899080c5ae06
Olgierd Humenczuk <olgierdh@gmail.com>
2013-10-15 23:25:26 +0200

    [refac] removed unused array

--- src/experimental/on_demand_processing_prototype/http_layer_data.h
+++ src/experimental/on_demand_processing_prototype/http_layer_data.h
@@ -16,7 +16,6 @@
 typedef struct
 {
     char                        parser_state;          //!< represents the state of the http layer parser parsing header, state, payload etc, etc.
-    char                        line_buffer[ 64 ];     //!< this buffer size must be equal to the size of the longest line to be processed via above layer
     unsigned char               last_char_marker;      //!< the marker that it's used to point to the last char in the buffer
     xi_stated_sscanf_state_t    xi_stated_sscanf_state;
     short                       counter;               //!< multiple purposes counter
